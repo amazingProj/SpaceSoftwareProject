49f940b88de10a311fb0735e2f536dc031077964
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2022-04-04 08:25:56 +0300

    initial commit

---
+++ PlanetsWizard/BL/BL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class BL : IBL
+    {
+        public BL()
+        {
+
+        }
+
+        public string UploadImage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
